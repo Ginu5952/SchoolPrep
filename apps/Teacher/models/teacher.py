791c55749f7ab61cbc0b2344ae12ac626e3adcb3
Neha-Gi <neha.girme@dci-student.org>
2024-10-07 14:53:26 +0200

    fixed teacher

--- apps/Teacher/models/teacher.py
+++ apps/Teacher/models/teacher.py
@@ -10,9 +10,14 @@ class Teacher(models.Model):
         return f"{self.user.first_name} {self.user.last_name}"
 
 
+
 class Class(models.Model):
     class_name = models.CharField(max_length=100)
     academic_year_start = models.IntegerField()
     academic_year_end = models.IntegerField()
     grade = models.IntegerField()
     teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE, default=1)
+   
+
+    def __str__(self):
+        return f"Class:{self.class_name} (Grade: {self.grade}, ID: {self.id})"
