5b99c18af73601e5a9b8c30aef870e441c4deea1
Neha-Gi <neha.girme@dci-student.org>
2024-10-08 11:57:15 +0200

    modifications

--- apps/Teacher/views/timetable.py
+++ apps/Teacher/views/timetable.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from apps.Teacher.models.timetable import TimeTable
-from apps.Teacher.serializers.timetable import TimeTableSerializer
+from apps.Teacher.serializer.timetable import TimeTableSerializer
 
 
 @api_view(["GET", "POST"])
