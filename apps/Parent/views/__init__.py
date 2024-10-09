f768e88fd6c56b5862d3d58adc7471ced57a6967
Sayli <saylinavale89@gmail.com>
2024-10-09 22:29:31 +0200

    View lunch menu working fine

--- apps/Parent/views/__init__.py
+++ apps/Parent/views/__init__.py
@@ -5,3 +5,4 @@ from .updateprofile import edit_profile
 from .leave import parent_leave_list_create
 from .writemsg import write_message
 from .viewtimetable import timetable_view
+from .viewlunchmenu import lunchmenu_view
